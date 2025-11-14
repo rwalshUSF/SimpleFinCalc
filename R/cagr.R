fea27549202d879771ccef0c159af9010c8e5b26
Robert Walsh <robertwalsh@usf.edu>
2025-11-14 17:49:42 -0500

    add cagr function

---
+++ R/cagr.R
@@ -0,0 +1,34 @@
+#' Compound Annual Growth Rate
+#'
+#' This function performs a calculation with robust input validation.
+#'
+#' @param start_value Single numeric value - The initial value of the investment.
+#' @param end_value Single numeric value - The final value of the investment.
+#' @param years Single numeric value - The number of years the investment was held.
+#'
+#' @returns The Compound Annual Growth Rate (CAGR) of an investment as a decimal.
+#' @export
+#'
+#' @examples
+#'
+#' # Using user variables:
+#'
+#' testSV<-1000
+#' testEV<-15632
+#' testY<-10
+#' fv_simple(testSV,testEV,testY)
+#'
+cagr <- function(start_value, end_value, years) {
+  # Input validation
+  if (!is.numeric(start_value) || any(is.na(start_value)) || length(start_value) != 1) {
+    stop("Argument 'start_value' must be a single numeric value without NA.")
+  }
+  if (!is.numeric(end_value) || any(is.na(end_value)) || length(end_value) != 1) {
+    stop("Argument 'end_value' must be a single numeric value without NA.")
+  }
+  if (!is.numeric(years) || any(is.na(years)) || length(years) != 1 || any(years <= 0)) {
+    stop("Argument 'years' must be a single numeric value without NA & positive.")
+  }
+  cagr_rate <- ((end_value / start_value)^(1 / years)) - 1
+  return(cagr_rate)
+}
---
+++ tests/testthat/test-cagr.R
@@ -0,0 +1,19 @@
+test_that("cagr() returns calculated value", {
+  expect_equal(cagr(1000,15632,10),0.316441166)
+})
+
+test_that("cagr() returns calculated value", {
+  expect_equal(cagr(profiles1$Start_Value[1],profiles1$End_Value[1],profiles1$Years[1]),0.033356201)
+})
+
+test_that("cagr() errors if principal != numeric", {
+  expect_error(cagr("One Thousand",5,10))
+})
+
+test_that("cagr() errors if rate != numeric", {
+  expect_error(cagr(1000,"Five",10))
+})
+
+test_that("cagr() errors if years != numeric", {
+  expect_error(cagr(1000,5,"Ten"))
+})
