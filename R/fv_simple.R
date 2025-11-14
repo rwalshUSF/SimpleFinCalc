c1eee22a8eb1963769ca3c69269f0f0674660491
Robert Walsh <robertwalsh@usf.edu>
2025-11-14 06:51:54 -0500

    add documentation for fv_simple

--- R/fv_simple.R
+++ R/fv_simple.R
@@ -1,4 +1,18 @@
-# Simple Future Value Function
+#' Simple Future Value
+#'
+#' @param principal Single numeric value
+#' @param rate Single numeric percentage
+#' @param years Single numeric value
+#'
+#' @returns A numeric value
+#' @export
+#'
+#' @examples
+#' testP<-1000
+#' testR<-5
+#' testY<-10
+#' fv_simple(testP,testR,testY)
+#'
 fv_simple <- function(principal, rate, years) {
   # Ensure rate is in decimal form
   if (rate > 1) {
