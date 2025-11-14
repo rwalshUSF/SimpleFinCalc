55d9f1d54368fe0c7948521cc57ac93c71db183e
Robert Walsh <robertwalsh@usf.edu>
2025-11-14 07:21:44 -0500

    add error tests

--- tests/testthat/test-fv_simple.R
+++ tests/testthat/test-fv_simple.R
@@ -1,3 +1,15 @@
-test_that("fv_simple returns calculated value", {
+test_that("fv_simple() returns calculated value", {
   expect_equal(fv_simple(1000,5,10),1628.89463)
+})
+
+test_that("fv_simple() errors if principal != numeric", {
+  expect_error(fv_simple("One Thousand",5,10))
+})
+
+test_that("fv_simple() errors if rate != numeric", {
+  expect_error(fv_simple(1000,"Five",10))
+})
+
+test_that("fv_simple() errors if years != numeric", {
+  expect_error(fv_simple(1000,5,"Ten"))
 })
