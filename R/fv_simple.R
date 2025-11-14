c4cec7bfe349df73ebdfa25a97f5f5be1e0bc33e
Robert Walsh <robertwalsh@usf.edu>
2025-11-14 09:51:18 -0500

    update fv_simple with defensive debugging

--- R/fv_simple.R
+++ R/fv_simple.R
@@ -1,19 +1,38 @@
 #' Simple Future Value
 #'
-#' @param principal Single numeric value
-#' @param rate Single numeric percentage
-#' @param years Single numeric value
+#' This function performs a calculation with robust input validation.
 #'
-#' @returns A numeric value
+#' @param principal Single numeric value - Must not contain `NA` values and all values must be positive.
+#' @param rate Single numeric percentage - Must not contain `NA` values and all values must be positive.
+#' @param years Single numeric value - Must not contain `NA` values and all values must be positive.
+#'
+#' @returns A numeric value representing the result of the calculation.
 #' @export
 #'
 #' @examples
+#'
+#' # Using user variables:
+#'
 #' testP<-1000
 #' testR<-5
 #' testY<-10
 #' fv_simple(testP,testR,testY)
 #'
+#' # Using built in 'profiles1' data set:
+#'
+#' fv_simple(profiles1$Principal[1],profiles1$Rate[1],profiles1$Years[1])
+#'
 fv_simple <- function(principal, rate, years) {
+  # Input validation
+  if (!is.numeric(principal) || any(is.na(principal)) || length(principal) != 1 || any(principal <= 0)) {
+    stop("Argument 'principal' must be a single numeric value without NA & positive.")
+  }
+  if (!is.numeric(rate) || any(is.na(rate)) || length(rate) != 1 || any(rate <= 0)) {
+    stop("Argument 'rate' must be a single numeric value without NA & positive.")
+  }
+  if (!is.numeric(years) || any(is.na(years)) || length(years) != 1 || any(years <= 0)) {
+    stop("Argument 'years' must be a single numeric value without NA & positive.")
+  }
   # Ensure rate is in decimal form
   if (rate > 1) {
     rate <- rate / 100
--- tests/testthat/test-fv_simple.R
+++ tests/testthat/test-fv_simple.R
@@ -2,6 +2,10 @@ test_that("fv_simple() returns calculated value", {
   expect_equal(fv_simple(1000,5,10),1628.89463)
 })
 
+test_that("fv_simple() returns calculated value", {
+  expect_equal(fv_simple(profiles1$Principal[1],profiles1$Rate[1],profiles1$Years[1]),10861782.0)
+})
+
 test_that("fv_simple() errors if principal != numeric", {
   expect_error(fv_simple("One Thousand",5,10))
 })
